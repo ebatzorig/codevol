705de07a40e6fa0aa1a2d2184794bb94602e0954
batzorig.enkh <batzorig.e@tdbm.mn>
2020-08-05 10:27:23 +0800

    Updates

---
+++ precache-manifest.0ee5ad1a35d825cab793e8890f9580c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c67877f8f799afa06160599c56112a",
+    "url": "/codevol/index.html"
+  },
+  {
+    "revision": "cd40a25c285b18b0f047",
+    "url": "/codevol/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7de614fc37157bed1d84",
+    "url": "/codevol/static/css/main.3c566a94.chunk.css"
+  },
+  {
+    "revision": "cd40a25c285b18b0f047",
+    "url": "/codevol/static/js/2.1524160f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/codevol/static/js/2.1524160f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de614fc37157bed1d84",
+    "url": "/codevol/static/js/main.7ea98270.chunk.js"
+  },
+  {
+    "revision": "e44ab4e3147be414bae0",
+    "url": "/codevol/static/js/runtime-main.c60a0514.js"
+  },
+  {
+    "revision": "4008b57fe205be8d0c5cc45705ee337e",
+    "url": "/codevol/static/media/devgrud.4008b57f.png"
+  },
+  {
+    "revision": "4b00c37aa01b12827bc9d628c2b1494e",
+    "url": "/codevol/static/media/everest.4b00c37a.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/codevol/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "210ddbd5491b2f693e80b340326f57e6",
+    "url": "/codevol/static/media/youtube.210ddbd5.png"
+  }
+]);
